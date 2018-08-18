b8562826394847b4dac609d9881716b046d6d162
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-08-18 16:29:16 +0200

    React preset and tasks Model

---
+++ database/migrations/2018_08_18_141656_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->integer('project_id')->unsigned();
+            $table->integer('label_id')->unsigned();
+            $table->dateTime('start_time')->nullable();
+            $table->dateTime('end_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
