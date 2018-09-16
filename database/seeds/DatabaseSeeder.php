74810386f0e70f7f5a31c9a49b2f5f77d3f718ce
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-09-16 14:46:40 +0200

    Disable seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        \App\User::create([
-            'name' => 'joe',
-            'email' => 'joe@example.com',
-            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-            'remember_token' => str_random(10),
-        ]);
+        // \App\User::create([
+        //     'name' => 'joe',
+        //     'email' => 'joe@example.com',
+        //     'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        //     'remember_token' => str_random(10),
+        // ]);
 
         $this->call(ColorsTableSeeder::class);
     }
