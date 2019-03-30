dfdf21f195ff45a32d67635a67754982a254a341
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-03-30 09:55:20 +0200

    basic testing

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Task;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
+
+// Remember to specify user_id when calling task factory.
 
 $factory->define(App\Task::class, function (Faker $faker) {
     return [
-        //
+        'description' => $faker->sentence,
+        'start_time' => Carbon::now()->toDateTimeString(),
+        'end_time' => Carbon::now()->toDateTimeString(),
+    ];
+});
+
+$factory->state(App\Task::class, 'active', function(Faker $faker) {
+    return [
+        'end_time' => null,
     ];
 });
--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,20 +12,57 @@ class TasksTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $user;
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    private function createTask($data) {
+        return $this->actingAs($this->user)->json('post', '/app/tasks', $data);
+    }
+
     /** @test */
     public function can_create_a_task()
     {
-//        $this->withoutExceptionHandling();
-        $user = factory(User::class)->create();
-
-        $data = [
-            'description' => 'fixing component state issue',
-        ];
-        $response = $this->actingAs($user)->json('post', '/app/tasks', $data);
+        $response = $this->actingAs($this->user)->json('post', '/app/tasks', ['description' => 'fixing component']);
 
         $response
             ->assertStatus(200)
             ->assertJson(['message' => 'success']);
-
     }
+
+    /** @test */
+    public function can_get_active_task()
+    {
+        factory(Task::class)->states(['active'])->create([
+            'user_id' => $this->user->id,
+            'description' =>  'my active task',
+        ]);
+
+        $response = $this->actingAs($this->user)->json('get', '/app/tasks/active');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(['task' => [
+                'user_id' => $this->user->id,
+                'description' => 'my active task',
+            ]]);
+    }
+
+    /** @test */
+    public function can_get_tasks()
+    {
+        factory(Task::class, 20)->create(['user_id' => $this->user->id]);
+
+        $response = $this->actingAs($this->user)->json('get', '/app/tasks', [], ['X-Requested-With' => 'XMLHttpRequest']);
+
+        $response->assertStatus(200);
+        $tasks = $response->decodeResponseJson()['tasks'];
+        $this->assertEquals(20, count($tasks));
+        $this->assertEquals($this->user->id, $tasks[0]['user_id']);
+    }
+
+
 }
