4313d4c593b63349ff76a88ba36372eef4c2580c
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-11-10 09:25:09 +0200

    fix user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,16 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        // \App\User::create([
-        //     'name' => 'joe',
-        //     'email' => 'joe@example.com',
-        //     'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        //     'remember_token' => str_random(10),
-        // ]);
+//         $this->call(UsersTableSeeder::class);
+//         User::create([
+//             'first_name' => 'joe',
+//             'last_name' => 'smith',
+//             'email' => 'joe@example.com',
+//             'password' => Hash::make('password'),
+//             'remember_token' => str_random(10),
+//         ]);
+//
+//         $this->call(ColorsTableSeeder::class);
 
-        // $this->call(ColorsTableSeeder::class);
-
-        $this->call(FreshTasksSeeder::class);
+//        $this->call(FreshTasksSeeder::class);
     }
 }
