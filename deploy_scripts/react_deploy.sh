b30165364483b233bbf8b353b269d50372bb2621
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-11-27 08:18:30 +0200

    Test deployment

--- deploy_scripts/react_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/forge/t4.crud.co.za/reactapp
-
-npm run build
-
-rm -r ../public/static
-cp -r build/static ../public/static
-cp -f build/index.html ../resources/views/backend/index.blade.php
