cecebbbdd7f0fecd614aeb2c8c584f54fb4f2ad5
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-11-27 07:29:23 +0200

    Merge deploy scripts

--- deploy_scripts/laravel_deploy.sh
+++ deploy_scripts/laravel_deploy.sh
@@ -2,6 +2,8 @@
 
 cd /home/forge/t4.crud.co.za
 git pull origin master
+
+# Laravel
 composer install --no-interaction --prefer-dist --optimize-autoloader
 echo "" | sudo -S service php7.3-fpm reload
 
@@ -9,3 +11,9 @@ if [ -f artisan ]
 then
     php artisan migrate --force
 fi
+
+# React
+npm run build
+rm -r ../public/static
+cp -r build/static ../public/static
+cp -f build/index.html ../resources/views/backend/index.blade.php
