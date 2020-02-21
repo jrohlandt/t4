7d910719484fe6ca57a953f6f2a1f5896439a3b1
Jeandré Rohlandt <jrohlandt@gmail.com>
2020-02-21 07:39:08 +0200

    Dockerize Laravel application for local development.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(UsersTableSeeder::class);
-//         User::create([
-//             'first_name' => 'joe',
-//             'last_name' => 'smith',
-//             'email' => 'joe@example.com',
-//             'password' => Hash::make('password'),
-//             'remember_token' => str_random(10),
-//         ]);
-//
-//         $this->call(ColorsTableSeeder::class);
+         User::create([
+             'first_name' => 'demo',
+             'last_name' => 'user',
+             'email' => 'demo@example.com',
+             'password' => Hash::make('password'),
+             'remember_token' => str_random(10),
+         ]);
+
+         $this->call(ColorsTableSeeder::class);
 
 //        $this->call(FreshTasksSeeder::class);
     }
